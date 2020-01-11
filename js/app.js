c1e841596f9f1f11d1aa8bc0caa7f2b5147ff38d
karlsemilla <karlsemilla1@gmail.com>
2020-01-10 18:40:45 -0700

    Possible finished 1 and 2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 // add code here -- do your data fetch and manipulations after DOM loaded
+fetch('http://www.randyconnolly.com/funwebdev/3rd/api/movie/movies.php')
+    .then(res => res.json())
+    .then(posts => console.log(posts))
+let hide = document.querySelector("#selector").classList.add("is-hidden");
