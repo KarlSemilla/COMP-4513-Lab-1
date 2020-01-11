20989b02e57ea4af3539d16ca0a47f77d342480a
karlsemilla <karlsemilla1@gmail.com>
2020-01-10 23:12:16 -0700

    Data retrieved, need to sort

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 // add code here -- do your data fetch and manipulations after DOM loaded
-fetch('http://www.randyconnolly.com/funwebdev/3rd/api/movie/movies.php')
+window.addEventListener('DOMContentLoaded', function(){
+    document.querySelector("#selector").classList.add("is-hidden");
+    fetch('http://www.randyconnolly.com/funwebdev/3rd/api/movie/movies.php')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            //let hideSearch = document.querySelector("#selector").classList.add("is-hidden");
+            let hideLoader = document.querySelector("#loader").classList.add("is-hidden");
+            let showSelector = document.querySelector("#selector").classList.remove("is-hidden");
+            data.sort();
+            console.log(data);
+
+        })
+        .catch(function(error){
+            //console.log("Shit's not working");
+        });
+});
+
+    /*    
+    .then(function(response){
+        return reponse.json();
+    })
     .then(res => res.json())
     .then(posts => console.log(posts))
-let hide = document.querySelector("#selector").classList.add("is-hidden");
+    */
+
